8d8a9ee39bf5d103e5bf983f144817cf675a3c4b
Bradshaw <mbradshaw@rti.org>
2023-06-16 20:28:59 -0400

    This new section of code creates a data frame for EDA. It combines data from various endpoints in the spoonacular API.

--- ST558_Project1_SpoonacularFunctions.R
+++ ST558_Project1_SpoonacularFunctions.R
@@ -100,5 +100,58 @@ recipes <- getRecipes(apiKey, query, cuisine, NULL, includeIngredients, excludeI
 
 # Nest step: - create data frame from our recipes of interest
 
+# pull together the data
+recipe_df1 <- data.frame()
 
+# Iterate over each recipe ID and pull information from different endpoints to create a new dataset
+for (recipe_id in recipes$id) {
+  # Retrieve recipe information for the current recipe ID
+  recipe_data <- getRecipeById(recipe_id)
+  
+  # Retrieve taste information for the current recipe ID
+  taste_info <- getRecipeTasteById(recipe_id)
+  
+  # Retrieve nutrition information for the current recipe ID
+  nutrition_info <- getRecipeNutritionById(recipe_id)
+  # Extract non-list objects from the recipe information and add them to the data frame
+  recipe_info <- list(
+    id = recipe_data$id,
+    title = recipe_data$title,
+    vegetarian = recipe_data$vegetarian,
+    vegan = recipe_data$vegan,
+    glutenFree = recipe_data$glutenFree,
+    dairyFree = recipe_data$dairyFree,
+    veryHealthy = recipe_data$veryHealthy,
+    cheap = recipe_data$cheap,
+    veryPopular = recipe_data$veryPopular,
+    sustainable = recipe_data$sustainable,
+    preparationMinutes = recipe_data$preparationMinutes,
+    cookingMinutes = recipe_data$cookingMinutes,
+    healthScore = recipe_data$healthScore,
+    pricePerServing = recipe_data$pricePerServing,
+    servings = recipe_data$servings,
+    readyInMinutes = recipe_data$readyInMinutes,
+    sourceUrl = recipe_data$sourceUrl,
+    sweetness = taste_info$sweetness,
+    saltiness = taste_info$saltiness,
+    sourness = taste_info$sourness,
+    bitterness = taste_info$bitterness,
+    savoriness = taste_info$savoriness,
+    fattiness = taste_info$fattiness,
+    spiciness = taste_info$spiciness,
+    calories = nutrition_info$calories,
+    carbs = nutrition_info$carbs,
+    fat = nutrition_info$fat,
+    protein = nutrition_info$protein,
+    query = query,
+    cuisine = cuisine,
+    diet = diet,
+    maxReadyTime = maxReadyTime
+  )
+  
+  # Append the recipe information to the data frame
+  recipe_df1 <- rbind(recipe_df1, recipe_info)
+}
 
+# Print the resulting data frame
+glimpse(recipe_df1)
